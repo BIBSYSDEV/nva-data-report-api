7f70e785ff08f2522d21348bf6102cf01de79260
brinxmat <789709+brinxmat@users.noreply.github.com>
2023-12-04 05:03:11 +0100

    Add support for logging

--- data-loading/src/main/java/no/sikt/nva/data/report/api/etl/DataLoader.java
+++ data-loading/src/main/java/no/sikt/nva/data/report/api/etl/DataLoader.java
@@ -1,5 +1,12 @@
 package no.sikt.nva.data.report.api.etl;
 
-public class DataLoader {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class DataLoader {
+    public static final Logger LOGGER = LoggerFactory.getLogger(DataLoader.class);
+
+    public DataLoader() {
+        LOGGER.info("Initializing DataLoader");
+    }
 }
--- data-loading/src/test/java/no/sikt/nva/data/report/api/etl/DataLoaderTest.java
+++ data-loading/src/test/java/no/sikt/nva/data/report/api/etl/DataLoaderTest.java
@@ -1,11 +1,15 @@
 package no.sikt.nva.data.report.api.etl;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import nva.commons.logutils.LogUtils;
 import org.junit.jupiter.api.Test;
 
 class DataLoaderTest {
 
     @Test
-    void dataLoadingExists() {
+    void shouldLogStuff() {
+        final var logAppender = LogUtils.getTestingAppenderForRootLogger();
         new DataLoader();
+        assertTrue(logAppender.getMessages().contains("Initializing DataLoader"));
     }
 }
