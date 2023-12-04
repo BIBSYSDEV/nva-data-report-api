b3a1ece6a864c64ec1bcc1c1ebcbf460bcde8d62
brinxmat <789709+brinxmat@users.noreply.github.com>
2023-12-04 04:48:47 +0100

    Add framework for dataloading

---
+++ data-loading/src/test/java/no/sikt/nva/data/report/api/etl/DataLoaderTest.java
@@ -0,0 +1,11 @@
+package no.sikt.nva.data.report.api.etl;
+
+import org.junit.jupiter.api.Test;
+
+class DataLoaderTest {
+
+    @Test
+    void dataLoadingExists() {
+        new DataLoader();
+    }
+}
