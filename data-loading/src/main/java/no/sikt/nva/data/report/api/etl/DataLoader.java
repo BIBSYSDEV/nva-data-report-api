bc963ba1ea6b491d211dd3eb638120fc23d66464
brinxmat <789709+brinxmat@users.noreply.github.com>
2023-12-04 06:11:03 +0100

    Accept events

--- data-loading/src/main/java/no/sikt/nva/data/report/api/etl/DataLoader.java
+++ data-loading/src/main/java/no/sikt/nva/data/report/api/etl/DataLoader.java
@@ -1,12 +1,20 @@
 package no.sikt.nva.data.report.api.etl;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DataLoader {
+public class DataLoader implements RequestHandler<SQSEvent, Void> {
     public static final Logger LOGGER = LoggerFactory.getLogger(DataLoader.class);
 
     public DataLoader() {
         LOGGER.info("Initializing DataLoader");
+    }
+
+    @Override
+    public Void handleRequest(SQSEvent input, Context context) {
+        return null;
     }
 }
