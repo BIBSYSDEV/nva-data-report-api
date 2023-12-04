c53fb862a9703bee077936a935bab00426dd25ff
Bruno Saxoni <789709+brinxmat@users.noreply.github.com>
2023-12-04 11:44:28 +0100

    Actually load data (#7)
    * Add order by to queries
    
    * Generate test data pairs for affiliation query
    
    * Add contributor testing
    
    * Add test data for funding
    
    * Add test data for identifier requests
    
    * Add test data for publication resposne
    
    * Refactor test-data filtering
    
    * linting
    
    * Add database setup for bad-request tests
    
    * Add changes from checkstyle
    
    * Remove unsused resources
    
    * Give variable a more coherent name
    
    * Remove dead code
    
    * Refactor Fake database connection
    
    * Inject environment
    
    * Migrate to log4j-slf4j2-impl
    
    * Add framework for dataloading
    
    * Add support for logging
    
    * Accept events
    
    * Linting

--- data-loading/src/main/java/no/sikt/nva/data/report/api/etl/DataLoader.java
+++ data-loading/src/main/java/no/sikt/nva/data/report/api/etl/DataLoader.java
@@ -1,5 +1,20 @@
 package no.sikt.nva.data.report.api.etl;
 
-public class DataLoader {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class DataLoader implements RequestHandler<SQSEvent, Void> {
+    public static final Logger LOGGER = LoggerFactory.getLogger(DataLoader.class);
+
+    public DataLoader() {
+        LOGGER.info("Initializing DataLoader");
+    }
+
+    @Override
+    public Void handleRequest(SQSEvent input, Context context) {
+        return null;
+    }
 }
---
+++ data-loading/src/main/java/no/sikt/nva/data/report/api/etl/PersistedResourceMessage.java
@@ -0,0 +1,7 @@
+package no.sikt.nva.data.report.api.etl;
+
+import java.net.URI;
+
+public record PersistedResourceMessage(URI resourceFileUri) {
+
+}
