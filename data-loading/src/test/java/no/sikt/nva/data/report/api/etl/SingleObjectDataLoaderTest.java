2c9daa46f483fb442e3af95a3ef7be902bc5c0b5
Mona Ullah <54892125+monaullah@users.noreply.github.com>
2024-01-12 15:55:40 +0100

    Np 45931 distinguish update types (#11)
    * shouldExtractAndLogOperationType
    
    * Refactor, enum for event type. New test shouldLogUnknownEventTypeIfEventTypeIsUnknown
    
    * Fix codacy issue: overloaded methods

--- data-loading/src/test/java/no/sikt/nva/data/report/api/etl/SingleObjectDataLoaderTest.java
+++ data-loading/src/test/java/no/sikt/nva/data/report/api/etl/SingleObjectDataLoaderTest.java
@@ -34,7 +34,7 @@ class SingleObjectDataLoaderTest {
         assertTrue(logAppender.getMessages().contains("Initializing DataLoader"));
     }
 
-    @ParameterizedTest(name = "should extract and log folderName {0}")
+    @ParameterizedTest(name = "Should extract and log folderName {0}")
     @ValueSource(strings = {RESOURCES_FOLDER, NVI_CANDIDATES_FOLDER})
     void shouldExtractAndLogObjectParentFolder(String folderName) {
         var key = UnixPath.of(folderName, randomString()).toString();
@@ -51,5 +51,24 @@ class SingleObjectDataLoaderTest {
         var event = new PersistedResourceEvent(BUCKET_NAME, key, SOME_OPERATION);
         handler.handleRequest(event, context);
         assertTrue(logAppender.getMessages().contains("No parent folder"));
+    }
+
+    @ParameterizedTest(name = "Should extract and log eventType type {0}")
+    @ValueSource(strings = {"PutObject", "DeleteObject"})
+    void shouldExtractAndLogOperationType(String eventType) {
+        var key = UnixPath.of(RESOURCES_FOLDER, randomString()).toString();
+        var event = new PersistedResourceEvent(BUCKET_NAME, key, eventType);
+        final var logAppender = LogUtils.getTestingAppenderForRootLogger();
+        handler.handleRequest(event, context);
+        assertTrue(logAppender.getMessages().contains("Event type: " + EventType.parse(eventType)));
+    }
+
+    @Test
+    void shouldLogUnknownEventTypeIfEventTypeIsUnknown() {
+        var key = UnixPath.of(RESOURCES_FOLDER, randomString()).toString();
+        var event = new PersistedResourceEvent(BUCKET_NAME, key, randomString());
+        final var logAppender = LogUtils.getTestingAppenderForRootLogger();
+        handler.handleRequest(event, context);
+        assertTrue(logAppender.getMessages().contains("Unknown event type: " + event.eventType()));
     }
 }
