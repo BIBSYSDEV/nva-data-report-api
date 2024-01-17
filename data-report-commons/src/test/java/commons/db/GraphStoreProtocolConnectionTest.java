014548704fd9467a6fab0e7afc33921fd02694d6
brinxmat <789709+brinxmat@users.noreply.github.com>
2024-01-17 19:55:14 +0100

    Initialize test database once

--- data-report-commons/src/test/java/commons/db/GraphStoreProtocolConnectionTest.java
+++ data-report-commons/src/test/java/commons/db/GraphStoreProtocolConnectionTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import commons.formatter.ResponseFormatter;
+import java.util.Random;
 import nva.commons.core.Environment;
 import nva.commons.logutils.LogUtils;
 import org.apache.jena.fuseki.main.FusekiServer;
@@ -12,32 +13,43 @@ import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.riot.Lang;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class GraphStoreProtocolConnectionTest {
 
     private static final String GSP_ENDPOINT = "/gsp";
-    private FusekiServer server;
-    private GraphStoreProtocolConnection dbConnection;
+    private static FusekiServer server;
+    private static GraphStoreProtocolConnection dbConnection;
 
-    @BeforeEach
-    void setUp() {
+    /**
+     * Tests re-use the same database. This is intentional as it is the case
+     * for the production environment;
+     */
+    @BeforeAll
+    static void setUp() {
         var dataSet = DatasetFactory.createTxnMem();
         initializeGraphServer(dataSet);
         var url = server.serverURL();
         dbConnection = new GraphStoreProtocolConnection(url, url, new Environment().readEnv("QUERY_PATH"));
     }
 
-    @AfterEach
-    void tearDown() {
+    @AfterAll
+    static void tearDown() {
         server.stop();
+    }
+
+    @AfterEach
+    void clearDatabase() {
+        dbConnection.delete();
     }
 
     @Test
     void shouldWrite() {
-        var triple = "<https://example.org/a> <https://example.org/b> <https://example.org/c> .";
+        var random = new Random().nextInt();
+        var triple = "<https://example.org/a> <https://example.org/b> <https://example.org/c" + random + "> .";
         dbConnection.write(triple, Lang.NTRIPLES);
         var query = QueryFactory.create("SELECT * WHERE { ?a ?b ?c }");
         var result = dbConnection.getResult(query, new TestFormatter());
@@ -61,7 +73,7 @@ class GraphStoreProtocolConnectionTest {
         assertNull(result);
     }
 
-    private void initializeGraphServer(Dataset dataSet) {
+    private static void initializeGraphServer(Dataset dataSet) {
         server = FusekiServer.create()
                      .add(GSP_ENDPOINT, dataSet)
                      .build();
