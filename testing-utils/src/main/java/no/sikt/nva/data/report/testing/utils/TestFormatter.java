10f5ad7b2a60fd18d3571e56a4cca00ff8b00505
Bruno Saxoni <789709+brinxmat@users.noreply.github.com>
2024-01-24 10:29:07 +0100

    Use hard-coded-context replacement (#27)
    * Use hard-coded-context replacement
    
    * Linting
    
    * Use a token that exists in the URI we attempt to match
    
    * Fix context error
    
    * Write human language not code in docs

--- testing-utils/src/main/java/no/sikt/nva/data/report/testing/utils/TestFormatter.java
+++ testing-utils/src/main/java/no/sikt/nva/data/report/testing/utils/TestFormatter.java
@@ -1,11 +1,13 @@
 package no.sikt.nva.data.report.testing.utils;
 
 import commons.formatter.ResponseFormatter;
+import java.util.stream.Collectors;
 import org.apache.jena.query.ResultSet;
 
 public final class TestFormatter implements ResponseFormatter {
 
     public TestFormatter() {
+        // Simple object constructor.
     }
 
     @Override
@@ -16,10 +18,29 @@ public final class TestFormatter implements ResponseFormatter {
             var vars = current.varNames();
             while (vars.hasNext()) {
                 var x = current.get(vars.next());
-                triples.append("<").append(x).append("> ");
+                if (x.isLiteral()) {
+                    triples.append('"')
+                        .append(x.asLiteral().getValue())
+                        .append("\"@")
+                        .append(x.asLiteral().getLanguage())
+                        .append(' ');
+                } else {
+                    triples.append("<").append(x).append("> ");
+                }
             }
-            triples.append(".");
+            triples.append('.').append(System.lineSeparator());
         }
-        return triples.isEmpty() ? null : triples.toString();
+        return triples.isEmpty()
+                   ? null
+                   : createSemiDeterministicOrderingOfTriples(triples);
+    }
+
+    private String createSemiDeterministicOrderingOfTriples(StringBuilder triples) {
+        return triples.toString().lines().sorted(this::compare)
+                   .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    private int compare(String s1, String s2) {
+        return Integer.compare(s1.length(), s2.length());
     }
 }
