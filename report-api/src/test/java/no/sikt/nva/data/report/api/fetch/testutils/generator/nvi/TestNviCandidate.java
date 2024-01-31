022ca4b94b000143f9c6a724c8b9cc498f7c26dc
monaullah <monaullah98@gmail.com>
2024-01-31 13:59:12 +0100

    Fix test setup publication id

--- report-api/src/test/java/no/sikt/nva/data/report/api/fetch/testutils/generator/nvi/TestNviCandidate.java
+++ report-api/src/test/java/no/sikt/nva/data/report/api/fetch/testutils/generator/nvi/TestNviCandidate.java
@@ -1,6 +1,5 @@
 package no.sikt.nva.data.report.api.fetch.testutils.generator.nvi;
 
-import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
 import static org.apache.commons.io.StandardLineSeparator.CRLF;
 import java.time.Instant;
 import java.util.List;
@@ -30,7 +29,7 @@ public record TestNviCandidate(String identifier,
     }
 
     public Model generateModel() {
-        var publicationDetails = new PublicationDetailsGenerator(randomUri().toString());
+        var publicationDetails = new PublicationDetailsGenerator(publicationDetails().id());
         publicationDetails().contributors().stream()
             .map(TestNviContributor::toModel)
             .forEach(publicationDetails::withNviContributor);
