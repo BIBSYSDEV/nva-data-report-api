52da351dfbcac8c4f3af67d34b11ba9c608a9534
Mona Ullah <54892125+monaullah@users.noreply.github.com>
2024-02-23 15:38:18 +0100

    HeaderConstants (#78)
    Move header constants to final class HeaderConstants #78

---
+++ report-api/src/test/java/no/sikt/nva/data/report/api/fetch/testutils/generator/HeaderConstants.java
@@ -0,0 +1,44 @@
+package no.sikt.nva.data.report.api.fetch.testutils.generator;
+
+public final class HeaderConstants {
+
+    public static final String PUBLICATION_ID = "publicationId";
+    public static final String CONTRIBUTOR_IDENTIFIER = "contributorIdentifier";
+    public static final String IS_APPLICABLE = "isApplicable";
+    public static final String PUBLICATION_IDENTIFIER = "publicationIdentifier";
+    public static final String PUBLICATION_CATEGORY = "publicationCategory";
+    public static final String PUBLICATION_TITLE = "publicationTitle";
+    public static final String CONTRIBUTOR_SEQUENCE_NUMBER = "contributorSequenceNumber";
+    public static final String CONTRIBUTOR_ROLE = "contributorRole";
+    public static final String CONTRIBUTOR_ID = "contributorId";
+    public static final String CONTRIBUTOR_NAME = "contributorName";
+    public static final String AFFILIATION_ID = "affiliationId";
+    public static final String AFFILIATION_NAME = "affiliationName";
+    public static final String INSTITUTION_ID = "institutionId";
+    public static final String FACULTY_ID = "facultyId";
+    public static final String DEPARTMENT_ID = "departmentId";
+    public static final String GROUP_ID = "groupId";
+    public static final String FUNDING_SOURCE = "fundingSource";
+    public static final String FUNDING_ID = "fundingId";
+    public static final String FUNDING_NAME = "fundingName";
+    public static final String CHANNEL_TYPE = "channelType";
+    public static final String CHANNEL_IDENTIFIER = "channelIdentifier";
+    public static final String CHANNEL_NAME = "channelName";
+    public static final String CHANNEL_ONLINE_ISSN = "channelOnlineIssn";
+    public static final String CHANNEL_PRINT_ISSN = "channelPrintIssn";
+    public static final String CHANNEL_LEVEL = "channelLevel";
+    public static final String INSTITUTION_POINTS = "institutionPoints";
+    public static final String INSTITUTION_APPROVAL_STATUS = "institutionApprovalStatus";
+    public static final String PUBLICATION_DATE = "publicationDate";
+    public static final String TOTAL_POINTS = "totalPoints";
+    public static final String PUBLICATION_TYPE_CHANNEL_LEVEL_POINTS = "publicationTypeChannelLevelPoints";
+    public static final String AUTHOR_SHARE_COUNT = "authorShareCount";
+    public static final String INTERNATIONAL_COLLABORATION_FACTOR = "internationalCollaborationFactor";
+    public static final String REPORTED_PERIOD = "reportedPeriod";
+    public static final String GLOBAL_APPROVAL_STATUS = "globalApprovalStatus";
+    public static final String STATUS = "status";
+
+    private HeaderConstants() {
+        // NO-OP
+    }
+}
