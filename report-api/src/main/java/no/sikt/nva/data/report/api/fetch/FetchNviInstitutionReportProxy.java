09d357ac4967740f6d06f0fa1daf465df12b8460
Mona Ullah <54892125+monaullah@users.noreply.github.com>
2024-03-14 14:19:15 +0100

    Np 46410  FetchNviInstitutionReportProxyHandler (#98)
    * shouldReturn401WhenUserDoesNotHaveManageNviAccessRight
    
    * validate access rights in handler
    
    * shouldLogRequestingUsersTopLevelOrganization
    
    * shouldExtractAndLogPathParameterReportingYear
    
    * extract and log reportingYear and topLevelOrganization
    
    * code cleanup
    
    * Add endpoint to open api spec
    
    * Update CognitoUserPool
    
    * Add FetchNviInstitutionReportProxyHandler in template
    
    * Mysteriously failing test: add logAppender.stop() in logging tests
    
    * Remove jsonld response

---
+++ report-api/src/main/java/no/sikt/nva/data/report/api/fetch/FetchNviInstitutionReportProxy.java
@@ -0,0 +1,64 @@
+package no.sikt.nva.data.report.api.fetch;
+
+import static com.google.common.net.MediaType.MICROSOFT_EXCEL;
+import static com.google.common.net.MediaType.OOXML_SHEET;
+import static no.sikt.nva.data.report.api.fetch.CustomMediaType.TEXT_CSV;
+import static no.sikt.nva.data.report.api.fetch.CustomMediaType.TEXT_PLAIN;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.google.common.net.MediaType;
+import java.net.URI;
+import java.util.List;
+import nva.commons.apigateway.AccessRight;
+import nva.commons.apigateway.ApiGatewayHandler;
+import nva.commons.apigateway.RequestInfo;
+import nva.commons.apigateway.exceptions.ApiGatewayException;
+import nva.commons.apigateway.exceptions.UnauthorizedException;
+import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FetchNviInstitutionReportProxy extends ApiGatewayHandler<Void, String> {
+
+    private static final Logger logger = LoggerFactory.getLogger(FetchNviInstitutionReport.class);
+    private static final String PATH_PARAMETER_REPORTING_YEAR = "reportingYear";
+
+    public FetchNviInstitutionReportProxy() {
+        super(Void.class);
+    }
+
+    @Override
+    protected List<MediaType> listSupportedMediaTypes() {
+        return List.of(TEXT_CSV, TEXT_PLAIN, MICROSOFT_EXCEL, OOXML_SHEET);
+    }
+
+    @Override
+    protected String processInput(Void unused, RequestInfo requestInfo, Context context) throws ApiGatewayException {
+        validateAccessRights(requestInfo);
+        var reportingYear = requestInfo.getPathParameter(PATH_PARAMETER_REPORTING_YEAR);
+        var topLevelOrganization = extractTopLevelOrganization(requestInfo);
+        logRequest(topLevelOrganization, reportingYear);
+        return null;
+    }
+
+    @Override
+    protected Integer getSuccessStatusCode(Void unused, String o) {
+        return HttpStatus.SC_OK;
+    }
+
+    private static String extractTopLevelOrganization(RequestInfo requestInfo) {
+        return requestInfo.getTopLevelOrgCristinId()
+                   .map(URI::toString)
+                   .orElse("UnknownRequestTopLevelOrganization");
+    }
+
+    private static void logRequest(String topLevelOrganization, String reportingYear) {
+        logger.info("NVI institution status report requested for organization: {}, reporting year: {}",
+                    topLevelOrganization, reportingYear);
+    }
+
+    private void validateAccessRights(RequestInfo requestInfo) throws UnauthorizedException {
+        if (!requestInfo.userIsAuthorized(AccessRight.MANAGE_NVI)) {
+            throw new UnauthorizedException();
+        }
+    }
+}
---
+++ report-api/src/test/java/no/sikt/nva/data/report/api/fetch/FetchNviInstitutionReportHandlerProxyTest.java
@@ -0,0 +1,94 @@
+package no.sikt.nva.data.report.api.fetch;
+
+import static no.unit.nva.testutils.RandomDataGenerator.objectMapper;
+import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
+import static nva.commons.apigateway.GatewayResponse.fromOutputStream;
+import static nva.commons.core.attempt.Try.attempt;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import no.sikt.nva.data.report.api.fetch.testutils.requests.FetchNviInstitutionReportProxyRequest;
+import no.unit.nva.commons.json.JsonUtils;
+import no.unit.nva.stubs.FakeContext;
+import no.unit.nva.testutils.HandlerRequestBuilder;
+import nva.commons.apigateway.AccessRight;
+import nva.commons.apigateway.GatewayResponse;
+import nva.commons.logutils.LogUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
+
+public class FetchNviInstitutionReportHandlerProxyTest {
+
+    private static final String SOME_YEAR = "2023";
+    private static final AccessRight SOME_ACCESS_RIGHT_THAT_IS_NOT_MANAGE_NVI = AccessRight.SUPPORT;
+    private FetchNviInstitutionReportProxy handler;
+
+    @BeforeEach
+    public void setup() {
+        handler = new FetchNviInstitutionReportProxy();
+    }
+
+    @Test
+    void shouldReturn401WhenUserDoesNotHaveManageNviAccessRight() throws IOException {
+        var request = new FetchNviInstitutionReportProxyRequest(SOME_YEAR, "text/plain");
+        var unAuthorizedRequest = generateHandlerRequest(request, SOME_ACCESS_RIGHT_THAT_IS_NOT_MANAGE_NVI,
+                                                         randomUri());
+        var output = new ByteArrayOutputStream();
+        var context = new FakeContext();
+        handler.handleRequest(unAuthorizedRequest, output, context);
+        var response = fromOutputStream(output, GatewayResponse.class);
+        var actualProblem = objectMapper.readValue(response.getBody(), Problem.class);
+        var expectedProblem = getExpectedProblem(context.getAwsRequestId());
+        assertEquals(expectedProblem, objectMapper.writeValueAsString(actualProblem));
+    }
+
+    @Test
+    void shouldLogRequestingUsersTopLevelOrganization() throws IOException {
+        var logAppender = LogUtils.getTestingAppenderForRootLogger();
+        var topLevelCristinOrgId = randomUri();
+        var request = generateHandlerRequest(new FetchNviInstitutionReportProxyRequest(SOME_YEAR, "text/plain"),
+                                             AccessRight.MANAGE_NVI, topLevelCristinOrgId);
+        var output = new ByteArrayOutputStream();
+        var context = new FakeContext();
+        handler.handleRequest(request, output, context);
+        assertTrue(logAppender.getMessages().contains("for organization: " + topLevelCristinOrgId));
+    }
+
+    @Test
+    void shouldExtractAndLogPathParameterReportingYear() throws IOException {
+        var logAppender = LogUtils.getTestingAppenderForRootLogger();
+        var request = generateHandlerRequest(new FetchNviInstitutionReportProxyRequest(SOME_YEAR, "text/plain"),
+                                             AccessRight.MANAGE_NVI, randomUri());
+        var output = new ByteArrayOutputStream();
+        var context = new FakeContext();
+        handler.handleRequest(request, output, context);
+        assertTrue(logAppender.getMessages().contains("reporting year: " + SOME_YEAR));
+    }
+
+    private static InputStream generateHandlerRequest(FetchNviInstitutionReportProxyRequest request,
+                                                      AccessRight accessRight,
+                                                      URI topLevelCristinOrgId)
+        throws JsonProcessingException {
+        return new HandlerRequestBuilder<InputStream>(JsonUtils.dtoObjectMapper)
+                   .withHeaders(request.acceptHeader())
+                   .withAccessRights(randomUri(), accessRight)
+                   .withPathParameters(request.pathParameters())
+                   .withTopLevelCristinOrgId(topLevelCristinOrgId)
+                   .build();
+    }
+
+    private static String getExpectedProblem(String requestId) {
+        return attempt(() -> objectMapper.writeValueAsString(Problem.builder()
+                                                                 .withStatus(Status.UNAUTHORIZED)
+                                                                 .withTitle("Unauthorized")
+                                                                 .withDetail("Unauthorized")
+                                                                 .with("requestId", requestId)
+                                                                 .build())).orElseThrow();
+    }
+}
