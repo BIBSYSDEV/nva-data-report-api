1a456c3569c4ee5ecb3e629449af10a746686c4c
monaullah <monaullah98@gmail.com>
2024-03-14 11:14:33 +0100

    extract and log reportingYear and topLevelOrganization

--- report-api/src/main/java/no/sikt/nva/data/report/api/fetch/FetchNviInstitutionReportProxy.java
+++ report-api/src/main/java/no/sikt/nva/data/report/api/fetch/FetchNviInstitutionReportProxy.java
@@ -6,6 +6,7 @@ import static no.sikt.nva.data.report.api.fetch.CustomMediaType.TEXT_CSV;
 import static no.sikt.nva.data.report.api.fetch.CustomMediaType.TEXT_PLAIN;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.google.common.net.MediaType;
+import java.net.URI;
 import java.util.List;
 import nva.commons.apigateway.AccessRight;
 import nva.commons.apigateway.ApiGatewayHandler;
@@ -13,8 +14,13 @@ import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
 import nva.commons.apigateway.exceptions.UnauthorizedException;
 import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FetchNviInstitutionReportProxy extends ApiGatewayHandler<Void, String> {
+
+    private static final Logger logger = LoggerFactory.getLogger(FetchNviInstitutionReport.class);
+    private static final String PATH_PARAMETER_REPORTING_YEAR = "reportingYear";
 
     @Override
     protected List<MediaType> listSupportedMediaTypes() {
@@ -28,7 +34,21 @@ public class FetchNviInstitutionReportProxy extends ApiGatewayHandler<Void, Stri
     @Override
     protected String processInput(Void unused, RequestInfo requestInfo, Context context) throws ApiGatewayException {
         validateAccessRights(requestInfo);
+        var reportingYear = requestInfo.getPathParameter(PATH_PARAMETER_REPORTING_YEAR);
+        var topLevelOrganization = extractTopLevelOrganization(requestInfo);
+        logRequest(topLevelOrganization, reportingYear);
         return null;
+    }
+
+    private static String extractTopLevelOrganization(RequestInfo requestInfo) {
+        return requestInfo.getTopLevelOrgCristinId()
+                   .map(URI::toString)
+                   .orElse("UnknownRequestTopLevelOrganization");
+    }
+
+    private static void logRequest(String topLevelOrganization, String reportingYear) {
+        logger.info("NVI institution status report requested for organization: {}, reporting year: {}",
+                    topLevelOrganization, reportingYear);
     }
 
     private void validateAccessRights(RequestInfo requestInfo) throws UnauthorizedException {
