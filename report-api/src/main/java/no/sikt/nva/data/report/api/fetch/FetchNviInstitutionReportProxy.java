e555b917dc57278ea6dcea1b5149a20c160444b4
Mona Ullah <54892125+monaullah@users.noreply.github.com>
2024-03-15 10:10:44 +0100

    Np 46410 FetchNviInstitutionReportProxy - fetch from report endpoint (#100)
    * shouldReturn401WhenUserDoesNotHaveManageNviAccessRight
    
    * validate access rights in handler
    
    * shouldLogRequestingUsersTopLevelOrganization
    
    * shouldExtractAndLogPathParameterReportingYear
    
    * extract and log reportingYear and topLevelOrganization
    
    * code cleanup
    
    * Add endpoint to open api spec
    
    * Update CognitoUserPool
    
    * Add FetchNviInstitutionReportProxyHandler in template
    
    * Mysteriously failing test: add logAppender.stop() in logging tests
    
    * shouldReturnExpectedContentType
    
    * Encode query param institutionId. Add path institution
    
    * Fix path nvi-approval in mocking. Refactor
    
    * Refactor. Still red test
    
    * Mock content type header in response
    
    * Use argument matcher any in backend client mocking
    
    * Add Todo test
    
    * Refactor: Add NviInstitutionReportClient
    
    * shouldReturnBase64EncodedOutputStreamWhenContentTypeIsExcel
    
    * setIsBase64EncodedIfContentTypeIsExcel
    
    * Add environment variables in template
    
    * Mark default constructor methods with jacoco

--- report-api/src/main/java/no/sikt/nva/data/report/api/fetch/FetchNviInstitutionReportProxy.java
+++ report-api/src/main/java/no/sikt/nva/data/report/api/fetch/FetchNviInstitutionReportProxy.java
@@ -4,15 +4,24 @@ import static com.google.common.net.MediaType.MICROSOFT_EXCEL;
 import static com.google.common.net.MediaType.OOXML_SHEET;
 import static no.sikt.nva.data.report.api.fetch.CustomMediaType.TEXT_CSV;
 import static no.sikt.nva.data.report.api.fetch.CustomMediaType.TEXT_PLAIN;
+import static nva.commons.core.attempt.Try.attempt;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.google.common.net.MediaType;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+import no.sikt.nva.data.report.api.fetch.client.NviInstitutionReportClient;
+import no.unit.nva.auth.AuthorizedBackendClient;
+import no.unit.nva.auth.CognitoCredentials;
+import no.unit.nva.auth.uriretriever.BackendClientCredentials;
 import nva.commons.apigateway.AccessRight;
 import nva.commons.apigateway.ApiGatewayHandler;
 import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
 import nva.commons.apigateway.exceptions.UnauthorizedException;
+import nva.commons.core.Environment;
+import nva.commons.core.JacocoGenerated;
+import nva.commons.secrets.SecretsReader;
 import org.apache.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,10 +29,23 @@ import org.slf4j.LoggerFactory;
 public class FetchNviInstitutionReportProxy extends ApiGatewayHandler<Void, String> {
 
     private static final Logger logger = LoggerFactory.getLogger(FetchNviInstitutionReport.class);
+    private static final String API_HOST = "API_HOST";
+    private static final String BACKEND_CLIENT_SECRET_NAME = "BACKEND_CLIENT_SECRET_NAME";
+    private static final String BACKEND_CLIENT_AUTH_URL = "COGNITO_HOST";
+    private static final String ACCEPT_HEADER = "Accept";
     private static final String PATH_PARAMETER_REPORTING_YEAR = "reportingYear";
+    private final NviInstitutionReportClient reportClient;
 
+    @JacocoGenerated
     public FetchNviInstitutionReportProxy() {
+        this(new NviInstitutionReportClient(
+            AuthorizedBackendClient.prepareWithCognitoCredentials(readCognitoCredentials()),
+            new Environment().readEnv(API_HOST)));
+    }
+
+    public FetchNviInstitutionReportProxy(NviInstitutionReportClient reportClient) {
         super(Void.class);
+        this.reportClient = reportClient;
     }
 
     @Override
@@ -36,13 +58,35 @@ public class FetchNviInstitutionReportProxy extends ApiGatewayHandler<Void, Stri
         validateAccessRights(requestInfo);
         var reportingYear = requestInfo.getPathParameter(PATH_PARAMETER_REPORTING_YEAR);
         var topLevelOrganization = extractTopLevelOrganization(requestInfo);
+        var acceptHeader = requestInfo.getHeader(ACCEPT_HEADER);
+        setIsBase64EncodedIfContentTypeIsExcel(acceptHeader);
         logRequest(topLevelOrganization, reportingYear);
-        return null;
+        return attempt(() -> reportClient.fetchReport(reportingYear, topLevelOrganization, acceptHeader)).orElseThrow();
     }
 
     @Override
     protected Integer getSuccessStatusCode(Void unused, String o) {
         return HttpStatus.SC_OK;
+    }
+
+    @JacocoGenerated
+    private static CognitoCredentials readCognitoCredentials() {
+        return Optional.of(
+                new SecretsReader().fetchClassSecret(BACKEND_CLIENT_SECRET_NAME, BackendClientCredentials.class))
+                   .map(FetchNviInstitutionReportProxy::mapToCognitoCredentials)
+                   .orElseThrow();
+    }
+
+    @JacocoGenerated
+    private static CognitoCredentials mapToCognitoCredentials(BackendClientCredentials secret) {
+        return new CognitoCredentials(secret::getId,
+                                      secret::getSecret,
+                                      readCognitoOAuthServerUri());
+    }
+
+    @JacocoGenerated
+    private static URI readCognitoOAuthServerUri() {
+        return URI.create(new Environment().readEnv(BACKEND_CLIENT_AUTH_URL));
     }
 
     private static String extractTopLevelOrganization(RequestInfo requestInfo) {
@@ -54,6 +98,16 @@ public class FetchNviInstitutionReportProxy extends ApiGatewayHandler<Void, Stri
     private static void logRequest(String topLevelOrganization, String reportingYear) {
         logger.info("NVI institution status report requested for organization: {}, reporting year: {}",
                     topLevelOrganization, reportingYear);
+    }
+
+    private static boolean isExcelOrOpenXml(String acceptHeader) {
+        return MICROSOFT_EXCEL.toString().equals(acceptHeader) || OOXML_SHEET.toString().equals(acceptHeader);
+    }
+
+    private void setIsBase64EncodedIfContentTypeIsExcel(String acceptHeader) {
+        if (isExcelOrOpenXml(acceptHeader)) {
+            setIsBase64Encoded(true);
+        }
     }
 
     private void validateAccessRights(RequestInfo requestInfo) throws UnauthorizedException {
--- report-api/src/test/java/no/sikt/nva/data/report/api/fetch/FetchNviInstitutionReportHandlerProxyTest.java
+++ report-api/src/test/java/no/sikt/nva/data/report/api/fetch/FetchNviInstitutionReportHandlerProxyTest.java
@@ -1,42 +1,65 @@
 package no.sikt.nva.data.report.api.fetch;
 
+import static java.net.HttpURLConnection.HTTP_OK;
 import static no.unit.nva.testutils.RandomDataGenerator.objectMapper;
+import static no.unit.nva.testutils.RandomDataGenerator.randomString;
 import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
 import static nva.commons.apigateway.GatewayResponse.fromOutputStream;
 import static nva.commons.core.attempt.Try.attempt;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.util.List;
+import java.util.Map;
+import no.sikt.nva.data.report.api.fetch.client.NviInstitutionReportClient;
 import no.sikt.nva.data.report.api.fetch.testutils.requests.FetchNviInstitutionReportProxyRequest;
+import no.unit.nva.auth.AuthorizedBackendClient;
 import no.unit.nva.commons.json.JsonUtils;
 import no.unit.nva.stubs.FakeContext;
 import no.unit.nva.testutils.HandlerRequestBuilder;
 import nva.commons.apigateway.AccessRight;
 import nva.commons.apigateway.GatewayResponse;
+import nva.commons.core.Environment;
 import nva.commons.logutils.LogUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.zalando.problem.Problem;
 import org.zalando.problem.Status;
 
 public class FetchNviInstitutionReportHandlerProxyTest {
 
+    private static final String TEXT_PLAIN = "text/plain";
+    private static final String TEXT_CSV = "text/csv";
+    private static final String OPEN_XML = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    private static final String EXCEL = "application/vnd.ms-excel";
     private static final String SOME_YEAR = "2023";
     private static final AccessRight SOME_ACCESS_RIGHT_THAT_IS_NOT_MANAGE_NVI = AccessRight.SUPPORT;
     private FetchNviInstitutionReportProxy handler;
+    private AuthorizedBackendClient authorizedBackendClient;
 
     @BeforeEach
     public void setup() {
-        handler = new FetchNviInstitutionReportProxy();
+        authorizedBackendClient = mock(AuthorizedBackendClient.class);
+        handler = new FetchNviInstitutionReportProxy(
+            new NviInstitutionReportClient(authorizedBackendClient, new Environment().readEnv("API_HOST")));
     }
 
     @Test
     void shouldReturn401WhenUserDoesNotHaveManageNviAccessRight() throws IOException {
-        var request = new FetchNviInstitutionReportProxyRequest(SOME_YEAR, "text/plain");
+        var request = new FetchNviInstitutionReportProxyRequest(SOME_YEAR, TEXT_PLAIN);
         var unAuthorizedRequest = generateHandlerRequest(request, SOME_ACCESS_RIGHT_THAT_IS_NOT_MANAGE_NVI,
                                                          randomUri());
         var output = new ByteArrayOutputStream();
@@ -51,24 +74,69 @@ public class FetchNviInstitutionReportHandlerProxyTest {
     @Test
     void shouldLogRequestingUsersTopLevelOrganization() throws IOException {
         var logAppender = LogUtils.getTestingAppenderForRootLogger();
-        var topLevelCristinOrgId = randomUri();
-        var request = generateHandlerRequest(new FetchNviInstitutionReportProxyRequest(SOME_YEAR, "text/plain"),
-                                             AccessRight.MANAGE_NVI, topLevelCristinOrgId);
+        var institutionId = randomUri();
+        var request = generateHandlerRequest(new FetchNviInstitutionReportProxyRequest(SOME_YEAR, TEXT_PLAIN),
+                                             AccessRight.MANAGE_NVI, institutionId);
         var output = new ByteArrayOutputStream();
         var context = new FakeContext();
         handler.handleRequest(request, output, context);
-        assertTrue(logAppender.getMessages().contains("for organization: " + topLevelCristinOrgId));
+        assertTrue(logAppender.getMessages().contains("for organization: " + institutionId));
     }
 
     @Test
     void shouldExtractAndLogPathParameterReportingYear() throws IOException {
         var logAppender = LogUtils.getTestingAppenderForRootLogger();
-        var request = generateHandlerRequest(new FetchNviInstitutionReportProxyRequest(SOME_YEAR, "text/plain"),
+        var request = generateHandlerRequest(new FetchNviInstitutionReportProxyRequest(SOME_YEAR, TEXT_PLAIN),
                                              AccessRight.MANAGE_NVI, randomUri());
         var output = new ByteArrayOutputStream();
         var context = new FakeContext();
         handler.handleRequest(request, output, context);
         assertTrue(logAppender.getMessages().contains("reporting year: " + SOME_YEAR));
+    }
+
+    //TODO: Add BadGatewayException test
+
+    @ParameterizedTest
+    @ValueSource(strings = {TEXT_CSV, TEXT_PLAIN, OPEN_XML, EXCEL})
+    void shouldReturnExpectedContentType(String contentType) throws IOException, InterruptedException {
+        var expectedResponseBody = randomString();
+        mockResponse(expectedResponseBody, contentType);
+        var output = new ByteArrayOutputStream();
+        var request = new FetchNviInstitutionReportProxyRequest(SOME_YEAR, contentType);
+        var handlerRequest = generateHandlerRequest(request, AccessRight.MANAGE_NVI, randomUri());
+        handler.handleRequest(handlerRequest, output, new FakeContext());
+        var response = fromOutputStream(output, GatewayResponse.class);
+        assertEquals(contentType, response.getHeaders().get("Content-Type"));
+        assertEquals(HTTP_OK, response.getStatusCode());
+        assertEquals(expectedResponseBody, response.getBody());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {OPEN_XML, EXCEL})
+    void shouldReturnBase64EncodedOutputStreamWhenContentTypeIsExcel(String contentType)
+        throws IOException, InterruptedException {
+        mockResponse(randomString(), contentType);
+        var output = new ByteArrayOutputStream();
+        var request = new FetchNviInstitutionReportProxyRequest(SOME_YEAR, contentType);
+        var handlerRequest = generateHandlerRequest(request, AccessRight.MANAGE_NVI, randomUri());
+        handler.handleRequest(handlerRequest, output, new FakeContext());
+        var response = fromOutputStream(output, GatewayResponse.class);
+        assertEquals(200, response.getStatusCode());
+        assertTrue(response.getIsBase64Encoded());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {TEXT_CSV, TEXT_PLAIN})
+    void shouldNotReturnBase64EncodedOutputStreamWhenContentTypeIsNotExcel(String contentType)
+        throws IOException, InterruptedException {
+        mockResponse(randomString(), contentType);
+        var output = new ByteArrayOutputStream();
+        var request = new FetchNviInstitutionReportProxyRequest(SOME_YEAR, contentType);
+        var handlerRequest = generateHandlerRequest(request, AccessRight.MANAGE_NVI, randomUri());
+        handler.handleRequest(handlerRequest, output, new FakeContext());
+        var response = fromOutputStream(output, GatewayResponse.class);
+        assertEquals(200, response.getStatusCode());
+        assertFalse(response.getIsBase64Encoded());
     }
 
     private static InputStream generateHandlerRequest(FetchNviInstitutionReportProxyRequest request,
@@ -90,5 +158,25 @@ public class FetchNviInstitutionReportHandlerProxyTest {
                                                                  .withDetail("Unauthorized")
                                                                  .with("requestId", requestId)
                                                                  .build())).orElseThrow();
+    }
+
+    private void mockResponse(String responseBody, String contentType)
+        throws IOException, InterruptedException {
+        var response = mockHttpResponse(responseBody, contentType);
+        mockResponse(response);
+    }
+
+    private void mockResponse(HttpResponse<String> response)
+        throws IOException, InterruptedException {
+        when(authorizedBackendClient.send(any(), any(BodyHandler.class))).thenReturn(response);
+    }
+
+    private HttpResponse<String> mockHttpResponse(String expectedResponse, String contentType) {
+        var response = mock(HttpResponse.class);
+        when(response.statusCode()).thenReturn(HTTP_OK);
+        when(response.headers()).thenReturn(HttpHeaders.of(Map.of("Content-Type", List.of(contentType)),
+                                                           (s, l) -> true));//?
+        when(response.body()).thenReturn(expectedResponse);
+        return response;
     }
 }
