822a0fd3b0003a41866930eed52fcb7b14ed0d9b
monaullah <monaullah98@gmail.com>
2024-03-19 13:20:58 +0100

    Code cleanup

--- report-api/src/test/java/no/sikt/nva/data/report/api/fetch/testutils/generator/nvi/TestApprovalStatus.java
+++ report-api/src/test/java/no/sikt/nva/data/report/api/fetch/testutils/generator/nvi/TestApprovalStatus.java
@@ -1,11 +1,7 @@
 package no.sikt.nva.data.report.api.fetch.testutils.generator.nvi;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum TestApprovalStatus {
     APPROVED("Approved"), PENDING("Pending"), REJECTED("Rejected");
-
-    @JsonValue
     private final String value;
 
     TestApprovalStatus(String value) {
