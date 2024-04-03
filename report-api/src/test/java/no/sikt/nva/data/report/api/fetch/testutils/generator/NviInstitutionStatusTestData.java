e0a570cdf285c8207e17b1d0444c9b1fd2f372f1
monaullah <monaullah98@gmail.com>
2024-04-03 08:32:23 +0200

    Remove stripping of trailing zeroes

--- report-api/src/test/java/no/sikt/nva/data/report/api/fetch/testutils/generator/NviInstitutionStatusTestData.java
+++ report-api/src/test/java/no/sikt/nva/data/report/api/fetch/testutils/generator/NviInstitutionStatusTestData.java
@@ -124,8 +124,8 @@ public final class NviInstitutionStatusTestData {
             .append(publication.getMainTitle()).append(DELIMITER)
             .append(LANGUAGE).append(DELIMITER)//TODO: Implement
             .append(getExpectedApprovalStatusValue(candidate.globalApprovalStatus())).append(DELIMITER)
-            .append(candidate.publicationTypeChannelLevelPoints().stripTrailingZeros()).append(DELIMITER)
-            .append(candidate.internationalCollaborationFactor().stripTrailingZeros()).append(DELIMITER)
+            .append(candidate.publicationTypeChannelLevelPoints()).append(DELIMITER)
+            .append(candidate.internationalCollaborationFactor()).append(DELIMITER)
             .append(AUTHOR_SHARE_COUNT).append(DELIMITER)//TODO: Implement
             .append(calculatePointsForAffiliation(affiliation, candidate, approval)).append(CRLF.getString());
     }
