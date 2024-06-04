b1b920f545201cb060c02a91a0c10fc87594b7b5
monaullah <monaullah98@gmail.com>
2024-06-04 08:53:28 +0200

    Add NviReportsBucket in template

--- report-api/src/main/java/no/sikt/nva/data/report/api/fetch/FetchNviInstitutionReportPresigner.java
+++ report-api/src/main/java/no/sikt/nva/data/report/api/fetch/FetchNviInstitutionReportPresigner.java
@@ -23,11 +23,11 @@ import software.amazon.awssdk.regions.Region;
 
 public class FetchNviInstitutionReportPresigner extends ApiS3PresignerGatewayHandler<Void> {
 
-    public static final String NVI_REPORTS_BUCKET = "NVI_REPORTS_BUCKET";
     private static final Logger logger = LoggerFactory.getLogger(FetchNviInstitutionReport.class);
     private static final Duration SIGN_DURATION = Duration.ofMinutes(60);
     private static final String REGION = "AWS_REGION";
     private static final String QUEUE_URL = "REPORT_QUEUE_URL";
+    private static final String NVI_REPORTS_BUCKET = "NVI_REPORTS_BUCKET";
     private final QueueClient queueClient;
 
     @JacocoGenerated
