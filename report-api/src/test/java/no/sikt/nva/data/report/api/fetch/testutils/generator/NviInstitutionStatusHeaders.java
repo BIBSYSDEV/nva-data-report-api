afd5482cdfad3a4a8f5cf67037eca8035c740706
monaullah <monaullah98@gmail.com>
2024-06-13 11:10:41 +0200

    Update nvi model with involvedOrganizations

--- report-api/src/test/java/no/sikt/nva/data/report/api/fetch/testutils/generator/NviInstitutionStatusHeaders.java
+++ report-api/src/test/java/no/sikt/nva/data/report/api/fetch/testutils/generator/NviInstitutionStatusHeaders.java
@@ -5,7 +5,7 @@ public final class NviInstitutionStatusHeaders {
     public static String PUBLICATION_IDENTIFIER = "VARBEIDLOPENR";
     public static String REPORTING_YEAR = "ARSTALL";
     public static String PUBLISHED_YEAR = "ARSTALL_REG";
-    public static String INSTITUTION_APPROVAL_STATUS = "STATUS_KONTROLLERT";//TODO: Check if this is correct
+    public static String INSTITUTION_APPROVAL_STATUS = "STATUS_KONTROLLERT";
     public static String PUBLICATION_INSTANCE = "PUBLIKASJONSFORM";
     public static String PUBLICATION_CHANNEL_TYPE = "PUBLISERINGSKANALTYPE";
     public static String ISSN = "ISSN";
@@ -15,20 +15,12 @@ public final class NviInstitutionStatusHeaders {
     public static final String FACULTY_ID = "AVDNR";
     public static final String DEPARTMENT_ID = "UNDAVDNR";
     public static final String GROUP_ID = "GRUPPENR";
-    public static final String AUTHOR_COUNT = "FORFATTERE_TOTALT";
-    public static final String AUTHOR_INT = "FORFATTERE_INT";//TODO: Find out what this is
-    public static final String PUBLICATION_TYPE_CHANNEL_LEVEL_POINTS = "VEKTET"; //TODO: Check if this is correct
     public static final String LAST_NAME = "ETTERNAVN";
     public static final String FIRST_NAME = "FORNAVN";
     public static final String PUBLICATION_CHANNEL_NAME = "PUBLISERINGSKANALNAVN";
-    public static final String PAGE_FROM = "SIDE_FRA";
-    public static final String PAGE_TO = "SIDE_TIL";
-    public static final String PAGE_COUNT = "SIDEANTALL";
     public static final String PUBLICATION_TITLE = "VA_TITTEL";
-    public static final String LANGUAGE = "SPRAK";
     public static final String GLOBAL_STATUS = "RAPPORTSTATUS";
-    public static final String TOTAL_POINTS = "VEKTINGSTALL"; //TODO: Check if this is correct
+    public static final String PUBLICATION_CHANNEL_LEVEL_POINTS = "VEKTINGSTALL";
     public static final String INTERNATIONAL_COLLABORATION_FACTOR = "FAKTORTALL_SAMARBEID";
-    public static final String AUTHOR_SHARE_COUNT = "FORFATTERDEL";
     public static final String POINTS_FOR_AFFILIATION = "FORFATTERVEKT";
 }
--- report-api/src/test/java/no/sikt/nva/data/report/api/fetch/testutils/generator/model/nvi/ApprovalGenerator.java
+++ report-api/src/test/java/no/sikt/nva/data/report/api/fetch/testutils/generator/model/nvi/ApprovalGenerator.java
@@ -1,5 +1,6 @@
 package no.sikt.nva.data.report.api.fetch.testutils.generator.model.nvi;
 
+import java.util.Set;
 import java.util.UUID;
 import no.sikt.nva.data.report.api.fetch.testutils.generator.Constants;
 import no.sikt.nva.data.report.api.fetch.testutils.generator.model.TripleBasedBuilder;
@@ -15,6 +16,8 @@ public class ApprovalGenerator extends TripleBasedBuilder {
     public static final PropertyImpl POINTS_PROPERTY = new PropertyImpl(Constants.ONTOLOGY_BASE_URI, "points");
     public static final PropertyImpl APPROVAL_STATUS = new PropertyImpl(Constants.ONTOLOGY_BASE_URI, "approvalStatus");
     public static final PropertyImpl INSTITUTION_ID = new PropertyImpl(Constants.ONTOLOGY_BASE_URI, "institutionId");
+    public static final Property INVOLVED_ORGANIZATION = new PropertyImpl(Constants.ONTOLOGY_BASE_URI,
+                                                                          "involvedOrganization");
     private static final Property APPROVAL = new PropertyImpl(Constants.ONTOLOGY_BASE_URI, "Approval");
     private final Model model;
     private final Resource subject;
@@ -38,6 +41,11 @@ public class ApprovalGenerator extends TripleBasedBuilder {
     public ApprovalGenerator withPoints(InstitutionPointsGenerator institutionPointsGenerator) {
         model.add(subject, POINTS_PROPERTY, institutionPointsGenerator.getSubject());
         model.add(institutionPointsGenerator.build());
+        return this;
+    }
+
+    public ApprovalGenerator withInvolvedOrganizations(Set<String> involvedOrganizations) {
+        involvedOrganizations.forEach(organization -> model.add(subject, INVOLVED_ORGANIZATION, organization));
         return this;
     }
 
