38e29aabbfb2fd322e87b68b025e4ec25b42ba52
brinxmat <789709+brinxmat@users.noreply.github.com>
2024-06-18 06:22:17 +0200

    Add view of publication

---
+++ bulk-load/src/main/java/no/sikt/nva/data/report/api/etl/transformer/ViewCompiler.java
@@ -0,0 +1,27 @@
+package no.sikt.nva.data.report.api.etl.transformer;
+
+import java.io.InputStream;
+import java.nio.file.Path;
+import nva.commons.core.ioutils.IoUtils;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+
+public class ViewCompiler {
+
+    private final Model model;
+
+    public ViewCompiler(InputStream inputStream) {
+        this.model = ModelFactory.createDefaultModel();
+        RDFDataMgr.read(model, inputStream, Lang.JSONLD);
+    }
+
+    public Model extractView() {
+        var query = IoUtils.stringFromResources(Path.of("view_of_publication.sparql"));
+        try (var queryExecution = QueryExecutionFactory.create(query, model)) {
+            return queryExecution.execConstruct();
+        }
+    }
+}
