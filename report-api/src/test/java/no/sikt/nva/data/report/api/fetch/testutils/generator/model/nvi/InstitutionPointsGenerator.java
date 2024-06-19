f8419d5516dcffd80fdd1bbcce8ceb4a8141d23f
brinxmat <789709+brinxmat@users.noreply.github.com>
2024-06-19 07:47:03 +0200

    Create valid blank nodes uniformly

--- report-api/src/test/java/no/sikt/nva/data/report/api/fetch/testutils/generator/model/nvi/InstitutionPointsGenerator.java
+++ report-api/src/test/java/no/sikt/nva/data/report/api/fetch/testutils/generator/model/nvi/InstitutionPointsGenerator.java
@@ -1,6 +1,5 @@
 package no.sikt.nva.data.report.api.fetch.testutils.generator.model.nvi;
 
-import java.util.UUID;
 import no.sikt.nva.data.report.api.fetch.testutils.generator.Constants;
 import no.sikt.nva.data.report.api.fetch.testutils.generator.model.TripleBasedBuilder;
 import org.apache.jena.rdf.model.Model;
@@ -20,7 +19,7 @@ public class InstitutionPointsGenerator extends TripleBasedBuilder {
 
     public InstitutionPointsGenerator() {
         this.model = ModelFactory.createDefaultModel();
-        this.subject = model.createResource("someBlankNode" + UUID.randomUUID());
+        this.subject = BlankNodeUtil.createRandom(model);
         model.add(subject, TYPE, INSTITUTION_POINTS);
     }
 
