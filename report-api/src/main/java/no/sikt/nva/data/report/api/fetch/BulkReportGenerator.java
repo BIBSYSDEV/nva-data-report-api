75d56fdb90acc2c703c69c582204af6e54b22d8e
monaullah <monaullah98@gmail.com>
2024-07-10 09:43:28 +0200

    Green test

--- report-api/src/main/java/no/sikt/nva/data/report/api/fetch/BulkReportGenerator.java
+++ report-api/src/main/java/no/sikt/nva/data/report/api/fetch/BulkReportGenerator.java
@@ -1,5 +1,6 @@
 package no.sikt.nva.data.report.api.fetch;
 
+import static java.util.Objects.isNull;
 import com.amazonaws.services.lambda.runtime.Context;
 import no.unit.nva.events.handlers.EventHandler;
 import no.unit.nva.events.models.AwsEventBridgeEvent;
@@ -11,8 +12,15 @@ public class BulkReportGenerator extends EventHandler<GenerateReportRequest, Voi
     }
 
     @Override
-    protected Void processInput(GenerateReportRequest generateReportRequest,
-                                AwsEventBridgeEvent<GenerateReportRequest> awsEventBridgeEvent, Context context) {
+    protected Void processInput(GenerateReportRequest request, AwsEventBridgeEvent<GenerateReportRequest> event,
+                                Context context) {
+        validate(request);
         return null;
+    }
+
+    private static void validate(GenerateReportRequest request) {
+        if (isNull(request.reportType())) {
+            throw new IllegalArgumentException("Report type is required");
+        }
     }
 }
