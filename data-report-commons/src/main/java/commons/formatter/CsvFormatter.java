17ce6272026cde0f3a4f4064c478a3bc6339f5a8
monaullah <monaullah98@gmail.com>
2024-08-14 15:44:49 +0200

    Use apache CSVPrinter

--- data-report-commons/src/main/java/commons/formatter/CsvFormatter.java
+++ data-report-commons/src/main/java/commons/formatter/CsvFormatter.java
@@ -1,10 +1,16 @@
 package commons.formatter;
 
-import static org.apache.commons.io.StandardLineSeparator.CRLF;
+import static java.util.Objects.isNull;
+import static nva.commons.core.StringUtils.EMPTY_STRING;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
 import java.util.List;
 import nva.commons.core.JacocoGenerated;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Literal;
@@ -13,76 +19,56 @@ import org.apache.jena.rdf.model.RDFNode;
 @JacocoGenerated //Tested in modules report-api and bulk-export
 public final class CsvFormatter implements ResponseFormatter {
 
-    public static final String DOUBLE_QUOTES = "\"";
     private static final String DECIMAL_PATTERN = "0.0000";
-    private static final String DELIMITER = ",";
     private static final String TYPE_INTEGER = "http://www.w3.org/2001/XMLSchema#integer";
     private static final String TYPE_DOUBLE = "http://www.w3.org/2001/XMLSchema#double";
-    private static final String LINE_BREAK = CRLF.getString();
 
     @Override
     public String format(ResultSet resultSet) {
         var resultVars = resultSet.getResultVars();
+        var writer = new StringWriter();
+        try (CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT)) {
+            printHeaders(csvPrinter, resultVars);
+            printRows(resultSet, resultVars, csvPrinter);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return writer.toString();
+    }
 
-        var csvBuilder = new StringBuilder();
-        appendHeaders(resultVars, csvBuilder);
-
+    private static void printRows(ResultSet resultSet, List<String> resultVars, CSVPrinter csvPrinter)
+        throws IOException {
         while (resultSet.hasNext()) {
-            appendResults(resultSet, resultVars, csvBuilder);
-        }
-
-        return csvBuilder.toString();
-    }
-
-    private static void appendHeaders(List<String> resultVars, StringBuilder csvBuilder) {
-        for (String var : resultVars) {
-            appendDouble(csvBuilder, var);
-        }
-        removeLastComma(csvBuilder);
-        appendLineBreak(csvBuilder);
-    }
-
-    private static void appendResults(ResultSet resultSet, List<String> resultVars, StringBuilder csvBuilder) {
-        var row = resultSet.next();
-        appendRow(resultVars, csvBuilder, row);
-        removeLastComma(csvBuilder);
-        appendLineBreak(csvBuilder);
-    }
-
-    private static void appendRow(List<String> resultVars, StringBuilder csvBuilder, QuerySolution row) {
-        for (String var : resultVars) {
-            var rdfNode = row.get(var);
-            if (rdfNode == null) {
-                appendEmptyString(csvBuilder);
-            } else if (isDoubleLiteral(rdfNode)) {
-                var formattedDouble = formatDouble(rdfNode.asLiteral());
-                appendDouble(csvBuilder, formattedDouble);
-            } else if (isIntegerLiteral(rdfNode)) {
-                appendInt(csvBuilder, rdfNode.asLiteral().getInt());
-            } else {
-                appendRdfNode(csvBuilder, rdfNode);
-            }
+            printRow(resultVars, csvPrinter, resultSet.next());
         }
     }
 
-    private static void appendEmptyString(StringBuilder csvBuilder) {
-        csvBuilder.append(DELIMITER);
+    private static void printRow(List<String> headers, CSVPrinter csvPrinter, QuerySolution querySolution)
+        throws IOException {
+        var rowData = new ArrayList<>();
+        headers.forEach(header -> buildRow(rowData, querySolution.get(header)));
+        csvPrinter.printRecord(rowData);
     }
 
-    private static void appendRdfNode(StringBuilder csvBuilder, RDFNode node) {
-        var nodeValue = node.toString();
-        if (nodeValue.contains(DELIMITER)) {
-            nodeValue = DOUBLE_QUOTES + nodeValue + DOUBLE_QUOTES;
+    private static void buildRow(ArrayList<Object> rowData, RDFNode rdfNode) {
+        addRdfNode(rdfNode, rowData);
+    }
+
+    private static void addRdfNode(RDFNode rdfNode, ArrayList<Object> rowData) {
+        if (isNull(rdfNode)) {
+            rowData.add(EMPTY_STRING);
+        } else if (isDoubleLiteral(rdfNode)) {
+            var formattedDouble = formatDouble(rdfNode.asLiteral());
+            rowData.add(formattedDouble);
+        } else if (isIntegerLiteral(rdfNode)) {
+            rowData.add(rdfNode.asLiteral().getInt());
+        } else {
+            rowData.add(rdfNode.toString());
         }
-        csvBuilder.append(nodeValue).append(DELIMITER);
     }
 
-    private static void appendDouble(StringBuilder csvBuilder, String value) {
-        csvBuilder.append(value).append(DELIMITER);
-    }
-
-    private static void appendInt(StringBuilder csvBuilder, int value) {
-        csvBuilder.append(value).append(DELIMITER);
+    private static void printHeaders(CSVPrinter csvPrinter, List<String> resultVars) throws IOException {
+        csvPrinter.printRecord(resultVars);
     }
 
     private static String formatDouble(Literal literal) {
@@ -107,13 +93,5 @@ public final class CsvFormatter implements ResponseFormatter {
 
     private static boolean isDouble(RDFNode rdfNode) {
         return TYPE_DOUBLE.equals(((Literal) rdfNode).getDatatypeURI());
-    }
-
-    private static void appendLineBreak(StringBuilder csvBuilder) {
-        csvBuilder.append(LINE_BREAK);
-    }
-
-    private static void removeLastComma(StringBuilder csvBuilder) {
-        csvBuilder.deleteCharAt(csvBuilder.length() - 1);
     }
 }
