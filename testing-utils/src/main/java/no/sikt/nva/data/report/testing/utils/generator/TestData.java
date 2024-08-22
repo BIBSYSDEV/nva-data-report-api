5de9128d511b79e11a5ffb8f64b3b23dae36edd6
monaullah <monaullah98@gmail.com>
2024-08-22 15:36:11 +0200

    Update deprecated queries

--- testing-utils/src/main/java/no/sikt/nva/data/report/testing/utils/generator/TestData.java
+++ testing-utils/src/main/java/no/sikt/nva/data/report/testing/utils/generator/TestData.java
@@ -32,6 +32,7 @@ import static no.sikt.nva.data.report.testing.utils.generator.PublicationHeaders
 import static no.sikt.nva.data.report.testing.utils.generator.PublicationHeaders.STATUS;
 import static no.unit.nva.testutils.RandomDataGenerator.randomUri;
 import static org.apache.commons.io.StandardLineSeparator.CRLF;
+import commons.ViewCompiler;
 import java.net.URI;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -253,6 +254,18 @@ public class TestData {
         return publication.getPublicationUri().equals(candidate.publicationDetails().id());
     }
 
+    private static Model getModelWithAppliedView(TestPublication publication) {
+        var id = URI.create(publication.getPublicationUri());
+        var model = publication.generateModel();
+        return new ViewCompiler(model).extractView(id);
+    }
+
+    private static Model getModelWithAppliedView(TestNviCandidate candidate) {
+        var id = URI.create(candidate.candidateUri());
+        var model = candidate.generateModel();
+        return new ViewCompiler(model).extractView(id);
+    }
+
     private void sortContributors(List<TestNviCandidate> expectedCandidates) {
         expectedCandidates.forEach(candidate -> candidate.publicationDetails()
                                                     .contributors()
@@ -287,13 +300,13 @@ public class TestData {
 
     private void addPublicationDataToModel(List<TestPublication> testData) {
         testData.stream()
-            .map(TestPublication::generateModel)
+            .map(TestData::getModelWithAppliedView)
             .forEach(models::add);
     }
 
     private void addNviDataToModel(List<TestNviCandidate> testData) {
         testData.stream()
-            .map(TestNviCandidate::generateModel)
+            .map(TestData::getModelWithAppliedView)
             .forEach(models::add);
     }
 
