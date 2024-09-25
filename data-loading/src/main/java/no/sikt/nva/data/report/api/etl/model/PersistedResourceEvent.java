b15f3c09e38ba60c82ee3cd9275e515cfbf83668
Mona Ullah <54892125+monaullah@users.noreply.github.com>
2024-09-25 15:26:50 +0200

    Fix PersistedResourcesEventRule, add sqs policy (#171)
    * Add SendMessagePolicy
    
    * Add resources to SendMessagePolicy
    
    * Fix SendMessagePolicy Resource
    
    * PersistedResourceCsvTransformer process input as SQSEvent

--- data-loading/src/main/java/no/sikt/nva/data/report/api/etl/model/PersistedResourceEvent.java
+++ data-loading/src/main/java/no/sikt/nva/data/report/api/etl/model/PersistedResourceEvent.java
@@ -1,10 +1,13 @@
 package no.sikt.nva.data.report.api.etl.model;
 
 import static java.util.Objects.isNull;
+import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
+import static nva.commons.core.attempt.Try.attempt;
+import no.unit.nva.commons.json.JsonSerializable;
 import nva.commons.core.JacocoGenerated;
 import nva.commons.core.paths.UnixPath;
 
-public record PersistedResourceEvent(String bucketName, String key, String eventType) {
+public record PersistedResourceEvent(String bucketName, String key, String eventType) implements JsonSerializable {
 
     public void validate() {
         validateBucketName();
@@ -24,6 +27,10 @@ public record PersistedResourceEvent(String bucketName, String key, String event
 
     public String getLocation() {
         return UnixPath.fromString(key).getParent().map(UnixPath::toString).orElse(null);
+    }
+
+    public static PersistedResourceEvent fromJson(String jsonString) {
+        return attempt(() -> dtoObjectMapper.readValue(jsonString, PersistedResourceEvent.class)).orElseThrow();
     }
 
     private static void validateParentFolder(PersistedResourceEvent input) {
