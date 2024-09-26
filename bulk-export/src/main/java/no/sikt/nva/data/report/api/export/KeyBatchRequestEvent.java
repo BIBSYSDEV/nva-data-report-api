05fb8dd91117d9f7aadd30f83a0fcaa7d2cfef7d
brinxmat <789709+brinxmat@users.noreply.github.com>
2024-09-26 14:37:50 +0200

    Move key batches handler

--- bulk-export/src/main/java/no/sikt/nva/data/report/api/export/KeyBatchRequestEvent.java
+++ bulk-export/src/main/java/no/sikt/nva/data/report/api/export/KeyBatchRequestEvent.java
@@ -1,4 +1,4 @@
-package no.sikt.nva.data.report.api.etl.transformer;
+package no.sikt.nva.data.report.api.export;
 
 import static java.util.Objects.nonNull;
 import com.fasterxml.jackson.annotation.JsonCreator;
